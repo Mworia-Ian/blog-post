264a6c8168feec52468360427c285564dac2b178
ianmmworia <ianmworia156@gmail.com>
2024-06-13 10:33:46 +0300

    added a post list component

---
+++ FrontEnd/src/components/Postlist.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/posts')
+      .then(response => setPosts(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
